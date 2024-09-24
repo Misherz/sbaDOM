235591c4474ad5c69ab7c9297f9e9c4bd0ebfc7b
Misherz <michelle.j.lugo@gmail.com>
2024-09-24 14:12:30 -0400

    Adding Dom features

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-// Cache using selectElementById.
-// Cache using querySelector/querySelectorAll.
-// Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
-// Iterate over a collection of elements to accomplish some task.
-// Create at least one element using createElement.
-// Use appendChild and/or prepend to add new elements to the DOM.
-// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 
-// Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
-// Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
-// Modify at least one attribute of an element in response to user interaction.
-// Register at least two different event listeners and create the associated event handler functions.
-// Use at least two Browser Object Model (BOM) properties or methods.
-// Include at least one form and/or input with HTML attribute validation.
-// Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
-// Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
-// Commit frequently to the git repository.
-// Include a README file that contains a description of your application.
-// Level of effort displayed in creativity, presentation, and user experience.
 
-//Windows Enter Prompt
- 
+//Position navBar to the right [get element by ID]
+let navBar = document.getElementById('mainNavBar');
+navBar.style.textAlign = "right";
+
+//Color for fieldsets[QuerySelectorALl]
+const fieldSets= document.querySelectorAll("fieldset");
+fieldSets.forEach(fset=>{
+    fset.style.backgroundColor="maroon";
+    fset.style.borderRadius="15px";
+    fset.style.padding="1em";
+
+})
+
+//Parent Node to navigate between elements
+
+//iterate over a collection of elements to complete a task
+
+//CreateElement
+//Append Child or Prepend
+// const button = document.createElement
+
+
